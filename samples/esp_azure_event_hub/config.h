aeee9093617ee56173526a4fba3a42402d944e73
dz <dzeyelid@gmail.com>
2016-02-17 09:16:12 +0900

    Added a sample sending sensor data to Azure Event Hub from ESP-WROOM-02.

---
+++ samples/esp_azure_event_hub/config.h
@@ -0,0 +1,9 @@
+const char* ssid = "YOUR-SSID";
+const char* password = "YOUR-SSID-PASSWORD";
+
+const char* host = "YOUR-EVENTHUB-ENDPOINT.servicebus.windows.net";
+const char* eventHubName = "YOUR-EVENTHUB-NAME";
+
+// Set SAS token that can be created at https://azure-event-hub-tool.mybluemix.net/
+const char* sas = "YOUR-SAS-TOKEN";
+
---
+++ samples/esp_azure_event_hub/esp_azure_event_hub.ino
@@ -0,0 +1,81 @@
+/*
+ *  Sending GPIO data to Azure Event Hub
+ */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+#include "config.h"
+
+// Use WiFiClientSecure class to create TLS connection
+WiFiClientSecure client;
+
+const int httpsPort = 443;
+
+// Set Endpoint to Azure Event Hub
+String url = String("/") + String(eventHubName) + String("/messages");
+String contentType = "application/atom+xml;type=entry;charset=utf-8";
+
+void sendData(String payload) {
+    String data = String("POST ") + url + " HTTP/1.1\r\n" +
+              "Host: " + String(host) + "\r\n" +
+              "Authorization: " + String(sas) + "\r\n" +
+              "Content-Type: " + String(contentType) + "\r\n" + 
+              "Content-Length: " + payload.length() + "\r\n" +
+              "\r\n" +
+              payload + "\r\n";
+  client.print(data);
+
+  Serial.println("request sent");
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
+  }
+  String line = client.readStringUntil('\n');
+  if (line.startsWith("{\"state\":\"success\"")) {
+    Serial.println("esp8266/Arduino CI successfull!");
+  } else {
+    Serial.println("esp8266/Arduino CI has failed");
+  }
+  Serial.println("reply was:");
+  Serial.println("==========");
+  Serial.println(line);
+  Serial.println("==========");
+  Serial.println("closing connection");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+}
+
+int value = 0;
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  String payload = "{lightResistance:" + String(sensorValue) + "}";
+  sendData(payload);
+  Serial.println(payload);
+  delay(1000);
+}
